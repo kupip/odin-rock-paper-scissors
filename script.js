7234f1846296d703c8eb322b6971bba5431a8cd7
Rafif Genadratama <muhammad.rafif.tif423@polban.ac.id>
2024-09-13 10:11:37 +0700

    finish getUserChoice

--- script.js
+++ script.js
@@ -1,2 +1,18 @@
-let choices = {"rock", "paper", "scissors"};
-console.log("Hello World");
+let choices = ["rock", "paper", "scissors"];
+
+function getComputerChoice() {
+  return choices[Math.floor(Math.random() * 3)];
+}
+
+function getUserChoice() {
+  let inp;
+  inp = prompt("What's your choice?\n(Rock, Paper, Scissors)");
+
+  String.toLowerCase(inp);
+  while (inp != "rock" && inp != "paper" && inp != "scissors") {
+    inp = prompt("Input the correct choice!");
+    String.toLowerCase();
+  }
+  return inp;
+}
+console.log(getComputerChoice());
