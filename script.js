09092a83efe1d848399f38b966fb6834241eeb73
Rafif Genadratama <muhammad.rafif.tif423@polban.ac.id>
2024-10-11 16:38:55 +0700

    finish game mechanism

--- script.js
+++ script.js
@@ -1,24 +1,49 @@
 let choices = ["rock", "paper", "scissors"];
 
 function getComputerChoice() {
-  return choices[Math.floor(Math.random() * 3)];
+  let choice;
+  choice = choices[Math.floor(Math.random() * 3)]
+  console.log(choice);
+  return choice;
 }
 
 function getUserChoice() {
   let inp;
   inp = prompt("What's your choice?\n(Rock, Paper, Scissors)");
 
-  String.toLowerCase(inp);
+  inp.toLowerCase();
   while (inp != "rock" && inp != "paper" && inp != "scissors") {
     inp = prompt("Input the correct choice!");
-    String.toLowerCase();
+    inp.toLowerCase();
   }
+  console.log(inp);
   return inp;
 }
 
-function determineWinner(com_inp, user_inp) {
-	if (user_inp == "rock") {
-
-	}
+function determineWinner(user_inp, com_inp) {
+  if (user_inp == com_inp) {
+    return "Tie!";
+  }
+  else {
+    if (user_inp == "rock") {
+      if (com_inp == "scissors") {
+	return "You win!";
+      } else {
+	return "Computer win!";
+      }
+    } else if (user_inp == "paper") {
+      if (com_inp == "rock") {
+	return "You win!";
+      } else {
+	return "Computer win!";
+      }
+    } else {
+      if (com_inp == "paper") {
+	return "You win!";
+      } else {
+	return "Computer win!";
+      }
+    }
+  }
 }
-console.log(getComputerChoice());
+console.log(determineWinner(getUserChoice(), getComputerChoice()));
